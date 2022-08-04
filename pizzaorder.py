ba1b7a9a2e3a73dd7676797584473a2e576059fb
Ntt37Vp <djkomong@protonmail.com>
2022-08-05 00:01:53 +0800

    initial commit

--- pizzaorder.py
+++ pizzaorder.py
@@ -1,5 +1,10 @@
 # Day 3 Exercise - Pizza Delivery
 print("Welcome to Python Pizza Delivery!")
-size = input("What size of pizza do you want? S, M, L or XL")
+size = input("What size of pizza do you want? S, M, L")
 add_pepperoni = input("Do you want pepperoni? Y or N.")
 add_cheese = input("Do you want to add cheese? Y or N")
+bill = 
+if size == S:
+
+
+print("Your final bill is ")
