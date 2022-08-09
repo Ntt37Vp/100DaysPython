8e07ef3093a3a8699b26eb0cae51e3a435596fbe
Ntt37Vp <djkomong@protonmail.com>
2022-08-10 04:13:30 +0800

    Day 7 files

---
+++ greet.py
@@ -0,0 +1,8 @@
+# Day 8 - Exercise 1
+# Function that allows input
+def greet_name(name):
+    print(f"Hello {name} I am a Greet function")
+    print("I was summoned by calling my function name")
+    print(f"I shall go now, {name}.")
+
+greet_name("Kevin")
