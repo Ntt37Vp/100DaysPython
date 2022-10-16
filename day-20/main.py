7b8a703fb0c3f30a66350976e5f8502ffd048e10
Ntt37Vp <fuckspammers@protonmail.com>
2022-10-16 23:24:12 +0800

    Day 20 continuation

--- day-20/main.py
+++ day-20/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,6 +26,23 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collection with FOOD
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with WALL
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 208 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with TAIL
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
