61d9e3ab0572fa251c0592ddd759c49f76f02c6f
Ntt37Vp <fuckspammers@protonmail.com>
2022-11-12 09:42:00 +0800

    Day 23 final

--- day-23/player.py
+++ day-23/player.py
@@ -4,6 +4,7 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+
 class Player(Turtle):
     def __init__(self):
         super().__init__()
@@ -11,13 +12,13 @@ class Player(Turtle):
         self.penup()
         self.go_to_start()
         self.setheading(90)
-    
+
     def go_up(self):
         self.forward(MOVE_DISTANCE)
 
     def go_to_start(self):
         self.goto(STARTING_POSITION)
-    
+
     def is_at_finish_line(self):
         if self.ycor() > FINISH_LINE_Y:
             return True
