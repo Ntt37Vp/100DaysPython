fa4deb4f09f77730de78b6355ddcdb5cf99cd654
Ntt37Vp <fuckspammers@protonmail.com>
2023-01-13 22:31:50 +0800

    day 32 start

---
+++ day-32-Birthday-Wisher/main.py
@@ -0,0 +1,12 @@
+# import smtplib
+#
+# my_email = "nortepedro294@gmail.com"
+# password = "hgarrnsctzecxxtx"
+#
+# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="dr.pedronorte@yahoo.com",
+#                         msg="Subject:Hello World!\n\nThis is the body of my email")
+
