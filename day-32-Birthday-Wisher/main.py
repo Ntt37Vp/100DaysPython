92f261b204378349f3d5381640648ce0c3204015
Ntt37Vp <fuckspammers@protonmail.com>
2023-01-14 23:53:11 +0800

    day 32 continuation

--- day-32-Birthday-Wisher/main.py
+++ day-32-Birthday-Wisher/main.py
@@ -8,5 +8,13 @@
 #     connection.login(user=my_email, password=password)
 #     connection.sendmail(from_addr=my_email,
 #                         to_addrs="dr.pedronorte@yahoo.com",
-#                         msg="Subject:Hello World!\n\nThis is the body of my email")
+#                         msg="Subject:Hello World!\n\nThis is the body of my email. Version 3")
 
+
+import datetime as dt
+now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of = now.weekday()
+
+date_of_birth = dt.datetime(year=, month=, day=)
