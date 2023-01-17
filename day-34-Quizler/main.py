de3f06012d3e8288094d214d0fc3e9da3f826e75
Ntt37Vp <fuckspammers@protonmail.com>
2023-01-17 11:09:11 +0800

    day 34 initial

---
+++ day-34-Quizler/main.py
@@ -0,0 +1,31 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+import requests
+from ui import QuizInterface
+
+api_url = "https://opentdb.com/api.php"
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get(api_url, params=parameters)
+response.raise_for_status()
+question_data = response.json()["results"]
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface()
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
---
+++ day-34-Quizler/ui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# Constant
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizler")
+
+        self.window.mainloop()
