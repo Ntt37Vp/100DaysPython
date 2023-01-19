e544c91504be532b17df4cf5cba029612f55c057
Ntt37Vp <fuckspammers@protonmail.com>
2023-01-19 12:58:36 +0800

    refactoring

--- day-33-APIs/ISS-Tracker-Capstone/main.py
+++ day-33-APIs/ISS-Tracker-Capstone/main.py
@@ -2,14 +2,8 @@ import requests
 from datetime import datetime
 import smtplib
 import time
+import constants
 
-# Constants
-MY_LAT = 10.315699
-MY_LONG = 123.885437
-MARGIN_ERROR = 5.0  # Position is within +5 or -5 degrees of the ISS position
-MY_USER = "nortepedro294@gmail.com"
-MY_PASS = "yzvtcxnhrbyjvfkj"
-MY_RECIPIENT = "dr.pedronorte@yahoo.com"
 
 
 # ISS locator
@@ -20,15 +14,15 @@ def is_iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    if abs(iss_latitude - MY_LAT) <= MARGIN_ERROR or abs(iss_longitude - MY_LONG) <= MARGIN_ERROR:
+    if abs(iss_latitude - constants.MY_LAT) <= constants.MARGIN_ERROR or abs(iss_longitude - constants.MY_LONG) <= constants.MARGIN_ERROR:
         return True
 
 
 # Checking Night
 def is_night():
     parameters = {
-        "lat": MY_LAT,
-        "lng": MY_LONG,
+        "lat": constants.MY_LAT,
+        "lng": constants.MY_LONG,
         "formatted": 0,
     }
 
@@ -48,9 +42,9 @@ while True:
     if is_iss_overhead() and is_night():
         connection = smtplib.SMTP("smtp.gmail.com")
         connection.starttls()
-        connection.login(user=MY_USER, password=MY_PASS)
+        connection.login(user=constants.MY_USER, password=constants.MY_PASS)
         connection.sendmail(
-            from_addr=MY_USER,
-            to_addrs=MY_RECIPIENT,
+            from_addr=constants.MY_USER,
+            to_addrs=constants.MY_RECIPIENT,
             msg="Subject:ISS Alert!\n\nHey Look Up, ISS is overhead!"
         )
