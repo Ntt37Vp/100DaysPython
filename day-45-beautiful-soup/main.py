7829b28e31a5a5e6bb4945743fd0178ee79d10ca
Ntt37Vp <fuckspammers@protonmail.com>
2023-02-08 09:52:52 +0800

    day 45 cont...

--- day-45-beautiful-soup/main.py
+++ day-45-beautiful-soup/main.py
@@ -1,33 +1,15 @@
 from bs4 import BeautifulSoup
+import requests
 
+response = requests.get("https://news.ycombinator.com/")
+yc_webpage = response.text
 
-with open("website.html") as file:
-    contents = file.read()
+soup = BeautifulSoup(yc_webpage, "html.parser")
 
-soup = BeautifulSoup(contents, "html.parser")
-# print(soup.title)
-# print(soup.title.string)
-# print(soup)
-# print(soup.prettify())
-# print(soup.a)
-anchor_tags = soup.find_all(name="a")
-paragraphs = soup.find_all(name="p")
-# print(anchor_tags)
-# print(paragraphs)
-# to get the strings inside the tags:
-# for tag in anchor_tags:
-    # print(tag.getText()) # to get the links tag
-    # print(tag.get("href")) # to get all links
-
-# heading = soup.find(name="h1", id="name")
-# print(heading)
-# section_heading = soup.find(name="h3", class_="heading")
-# print(section_heading)
-
-# get hold of specific element (similar to CSS approach of specificity)
-# company_url = soup.select_one(selector="p a")
-# print(company_url)
-# company = soup.select_one(selector="#name") # pound sign for the ID name
-# print(company)
-headings = soup.select(selector=".heading") # dot sign for the class heading
-print(headings)
+headline_tags = soup.find(class_="titleline")
+article_text = headline_tags.get_text()
+article_link = headline_tags.get("href")
+article_votes = soup.find(name="span", class_="score").get_text()
+print(article_text)
+print(article_link)
+print(article_votes)
