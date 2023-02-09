bdcc909801c3301f0e941234dba7ae46f726fbbe
Ntt37Vp <fuckspammers@protonmail.com>
2023-02-09 21:26:23 +0800

    day 45 refactor

--- day-45-beautiful-soup/main.py
+++ day-45-beautiful-soup/main.py
@@ -2,14 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://news.ycombinator.com/")
-yc_webpage = response.text
+soup = BeautifulSoup(response.text, "html.parser")
 
-soup = BeautifulSoup(yc_webpage, "html.parser")
+article_tags = soup.find_all(class_="titleline")
 
-headline_tags = soup.find(class_="titleline")
-article_text = headline_tags.get_text()
-article_link = headline_tags.get("href")
-article_votes = soup.find(name="span", class_="score").get_text()
-print(article_text)
-print(article_link)
+article_texts = []
+article_links = []
+for article in article_tags:
+    text = article_tags.getText()
+    article_texts.append(text)
+    link = article_tags.get("href")
+    article_links.append(link)
+
+article_votes = soup.find_all(name="span", class_="score").getText()
+
+print(article_texts)
+print(article_links)
 print(article_votes)
