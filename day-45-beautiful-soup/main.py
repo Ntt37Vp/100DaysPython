566d19c54dad33f938fea7ee0e43dea19691ad34
Ntt37Vp <fuckspammers@protonmail.com>
2023-02-12 06:42:16 +0800

    day 45 cont...

--- day-45-beautiful-soup/main.py
+++ day-45-beautiful-soup/main.py
@@ -2,20 +2,32 @@ from bs4 import BeautifulSoup
 import requests
 
 response = requests.get("https://news.ycombinator.com/")
-soup = BeautifulSoup(response.text, "html.parser")
+yc_data = response.text
 
-article_tags = soup.find_all(class_="titleline")
+soup = BeautifulSoup(yc_data, 'html.parser')
 
 article_texts = []
 article_links = []
-for article in article_tags:
-    text = article_tags.getText()
-    article_texts.append(text)
-    link = article_tags.get("href")
-    article_links.append(link)
+article_scores = []
 
-article_votes = soup.find_all(name="span", class_="score").getText()
+# Get all span tags containing articles
+spans = soup.find_all("span", {"class": "titleline"})
+# Get all span tangs containing scores
+spans_score = soup.find_all("span", {"class": "score"})
+
+# Get all article texts and url from spans containing articles
+article_texts = [a.find('a').getText() for a in spans]
+article_links = [a.find('a').get("href") for a in spans]
+# Get all scores from spans containing scores
+article_scores = [int(score.getText().split()[0]) for score in spans_score]
 
 print(article_texts)
 print(article_links)
-print(article_votes)
+print(article_scores)
+
+# Get index of highest score
+index = article_scores.index(max(article_scores))
+
+# Print story title, link and votes of story with highest votes.
+print(
+    f"Story with highest upvotes.\nTitle: {article_texts[index]}\nUrl: {article_links[index]}\nVotes: {article_scores[index]}")
