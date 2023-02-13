96b9f5d728e48ef3f304e0a60a63c0750325998f
Ntt37Vp <fuckspammers@protonmail.com>
2023-02-13 19:39:08 +0800

    day 45 cont...

--- day-45-beautiful-soup/main.py
+++ day-45-beautiful-soup/main.py
@@ -12,7 +12,7 @@ article_scores = []
 
 # Get all span tags containing articles
 spans = soup.find_all("span", {"class": "titleline"})
-# Get all span tangs containing scores
+# Get all span tags containing scores
 spans_score = soup.find_all("span", {"class": "score"})
 
 # Get all article texts and url from spans containing articles
@@ -21,13 +21,13 @@ article_links = [a.find('a').get("href") for a in spans]
 # Get all scores from spans containing scores
 article_scores = [int(score.getText().split()[0]) for score in spans_score]
 
-print(article_texts)
-print(article_links)
-print(article_scores)
+# print(article_texts)
+# print(article_links)
+# print(article_scores)
 
-# Get index of highest score
+# Get index of the highest score
 index = article_scores.index(max(article_scores))
 
-# Print story title, link and votes of story with highest votes.
+# Print story title, link and votes of story with the highest votes.
 print(
-    f"Story with highest upvotes.\nTitle: {article_texts[index]}\nUrl: {article_links[index]}\nVotes: {article_scores[index]}")
+    f"Story with highest upvotes:\nTitle: {article_texts[index]}\nUrl: {article_links[index]}\nVotes: {article_scores[index]}")
