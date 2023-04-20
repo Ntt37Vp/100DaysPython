9637630a32dd301de9f359780c69541580b7f450
Ntt37Vp <fuckspammers@protonmail.com>
2023-04-20 08:14:11 +0800

    day 61 sec 465

--- day-61-FormsWTF/main.py
+++ day-61-FormsWTF/main.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
+
 
 app = Flask(__name__)
+app.secret_key = "4fbiq$xYPXP@fELBsBJ"
 
 
 @app.route('/')
@@ -8,9 +17,18 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
