e6d69d762d41d98241e9c6e2bc49c3661c3fad9e
Ntt37Vp <fuckspammers@protonmail.com>
2023-04-25 01:46:08 +0800

    section 470 adding bootstrap-flask

--- day-61-FormsWTF/main.py
+++ day-61-FormsWTF/main.py
@@ -2,28 +2,30 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap4
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label='Log In')
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
 app.secret_key = "4fbiq$xYPXP@fELBsBJ"
+bootstrap = Bootstrap4(app)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
     login_form.validate_on_submit
-    return render_template('login.html', form=login_form)
+    return render_template("login.html", form=login_form)
 
 
 """ @app.route('/submit', methods=['GET', 'POST'])
@@ -34,5 +36,5 @@ def submit():
     return render_template('login.html', form=login_form) """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
