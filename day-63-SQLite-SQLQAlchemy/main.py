008ea25c0207af64317cde61a409b9fdcb9f830b
Ntt37Vp <fuckspammers@protonmail.com>
2023-04-27 10:33:29 +0800

    sec 479

--- day-63-SQLite-SQLQAlchemy/main.py
+++ day-63-SQLite-SQLQAlchemy/main.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
 
 app = Flask(__name__)
 
 all_books = []
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
 
 @app.route('/')
@@ -26,4 +31,3 @@ def add():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
