8788efaffca3893f46abbb806d5e536d4710d4e4
Ntt37Vp <fuckspammers@protonmail.com>
2023-04-28 20:18:01 +0800

    day 63 sec 482

--- day-63-SQLite-SQLQAlchemy/main.py
+++ day-63-SQLite-SQLQAlchemy/main.py
@@ -1,31 +1,64 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+
+# import sqlite3
 
 
 app = Flask(__name__)
-
 all_books = []
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+
+# CREATE DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+# So, instead of using sqlite we would use SQLAlchemy
+# db = sqlite3.connect("books-collection.db")
+# cursor = db.cursor()
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 
 
-@app.route('/')
+# CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+
+db.create_all()
+
+
+# CREATE RECORD
+new_book = Book(title="Harry Potter", author="JKR", rating=7)
+# not passing the id attribute would set it to auto-increment
+db.session.add(new_book)
+db.session.commit()
+
+
+@app.route("/")
 def home():
     return render_template("index.html", books=all_books)
 
 
-@app.route("/add", methods=['GET', 'POST'])
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method == 'POST':
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating":request.form["rating"]
-        }
-        all_books.append(new_book)
+    if request.method == "POST":
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"],
+        )
+        db.session.add(new_book)
+        db.session.commit()
+
         # Optional redirect after adding
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
     return render_template("add.html")
 
 
