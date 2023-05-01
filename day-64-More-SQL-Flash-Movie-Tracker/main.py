db1ba59a5e2aae6b8ea03815ce640f9c86914466
Ntt37Vp <fuckspammers@protonmail.com>
2023-05-01 08:05:31 +0800

    day 64 sec 485

--- day-64-More-SQL-Flash-Movie-Tracker/main.py
+++ day-64-More-SQL-Flash-Movie-Tracker/main.py
@@ -6,6 +6,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
@@ -30,20 +31,6 @@ class Movie(db.Model):
 
 with app.app_context():
     db.create_all()
-
-
-# Add test movie
-# new_movie = Movie(
-#     title = "Phone Booth",
-#     year = 2002,
-#     description = "Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper...",
-#     rating = 7.3,
-#     ranking = 1,
-#     review = "Meh",
-#     img_url = "https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# db.session.add(new_movie)
-# db.session.commit()
 
 
 @app.route("/")
