dd7dbee0f44e0c0b83c891a2fe172e99feda41d9
Ntt37Vp <fuckspammers@protonmail.com>
2023-05-04 10:36:44 +0800

    day 64 sec 489 more

--- day-64-More-SQL-Flash-Movie-Tracker/main.py
+++ day-64-More-SQL-Flash-Movie-Tracker/main.py
@@ -6,7 +6,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
@@ -95,7 +94,18 @@ def find_movie():
     movie_api_id = request.args.get("id")
     if movie_api_id:
         movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
-
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            # The data in release_date includes month and day, we will want to get rid of.
+            year=data["release_data"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
