632d1485e486aa724487999d9246ece386f85539
Ntt37Vp <fuckspammers@protonmail.com>
2023-05-09 20:39:28 +0800

    day 66 sec 502

--- day-66-REST/main.py
+++ day-66-REST/main.py
@@ -25,6 +25,10 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+def to_dict(self):
+    dic = {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    return dic
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -41,10 +45,20 @@ def get_random_cafe():
 @app.route("/all")
 def all_cafes():
     cafes = db.session.query(Cafe).all()
-
+    cafe_list = [to_dict(cafe) for cafe in cafes]
+    return jsonify(cafe_list)
 
 
 # HTTP PUT/PATCH - Update Record
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not found":"Sorry no cafe at that location"})
+
 
 # HTTP DELETE - Delete Record
 
