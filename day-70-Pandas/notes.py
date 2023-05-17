266635edcf0ae7e634bfce276dba8581b8d2f25e
Ntt37Vp <fuckspammers@protonmail.com>
2023-05-17 19:19:15 +0800

    day 70 - Pandas

---
+++ day-70-Pandas/notes.py
@@ -0,0 +1,8 @@
+# Use Google Collabotary 
+# https://colab.research.google.com/
+# https://pandas.pydata.org/Pandas_Cheat_Sheet.pdf
+
+# import pandas as pd
+# df = pd.read_csv('salaries_by_college_major.csv')
+# df.head()
+
