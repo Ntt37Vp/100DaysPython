30ec8278d79b1ebe777e036c159a0edad43a839b
Ntt37Vp <fuckspammers@protonmail.com>
2023-05-19 21:32:20 +0800

    completed day 71

--- day-70-71-Pandas/notes.py
+++ day-70-71-Pandas/notes.py
@@ -1,4 +1,4 @@
-# Use Google Collabotary 
+# Use Google Collabotary
 # https://colab.research.google.com/
 # https://pandas.pydata.org/Pandas_Cheat_Sheet.pdf
 
@@ -9,10 +9,10 @@
 # df.tail()
 # df.shape
 # df.columns
-    # Index(['Undergraduate Major', 'Starting Median Salary',
-    #        'Mid-Career Median Salary', 'Mid-Career 10th Percentile Salary',
-    #        'Mid-Career 90th Percentile Salary', 'Group'],
-    #       dtype='object')
+# Index(['Undergraduate Major', 'Starting Median Salary',
+#        'Mid-Career Median Salary', 'Mid-Career 10th Percentile Salary',
+#        'Mid-Career 90th Percentile Salary', 'Group'],
+#       dtype='object')
 
 # df.isna()
 # clean_df = df.dropna()
@@ -29,7 +29,7 @@
 # CODE CHALLENGE
 # What college major has the highest mid-career salary? How much do graduates with this major earn?
 # Which college major has the lowest starting salary and how much do graduates earn after university?
-# Which college major has the lowest mid-career salary and how much can people expect to earn with this degree? 
+# Which college major has the lowest mid-career salary and how much can people expect to earn with this degree?
 
 # SOLUTION
 # print(clean_df['Mid-Career Median Salary'].max())
@@ -41,32 +41,40 @@
 # clean_df.loc[clean_df['Mid-Career Median Salary'].idxmin()]
 
 # Sorting Values & Adding Columns: Majors with the Most Potential vs Lowest Risk
-    # clean_df['Mid-Career 90th Percentile Salary'] - clean_df['Mid-Career 10th Percentile Salary']
-    # OR USE subtract()
-    # clean_df['Mid-Career 90th Percentile Salary'].subtract(clean_df['Mid-Career 10th Percentile Salary'])
+# clean_df['Mid-Career 90th Percentile Salary'] - clean_df['Mid-Career 10th Percentile Salary']
+# OR USE subtract()
+# clean_df['Mid-Career 90th Percentile Salary'].subtract(clean_df['Mid-Career 10th Percentile Salary'])
 
-    # spread_col = clean_df['Mid-Career 90th Percentile Salary'] - clean_df['Mid-Career 10th Percentile Salary']
-    # clean_df.insert(1, 'Spread', spread_col)
-    # clean_df.head()
+# spread_col = clean_df['Mid-Career 90th Percentile Salary'] - clean_df['Mid-Career 10th Percentile Salary']
+# clean_df.insert(1, 'Spread', spread_col)
+# clean_df.head()
 
 # Sorting by the Lowest Spread
-    # low_risk = clean_df.sort_values('Spread')
-    # low_risk[['Undergraduate Major', 'Spread']].head()
+# low_risk = clean_df.sort_values('Spread')
+# low_risk[['Undergraduate Major', 'Spread']].head()
 
 
-# CODING CHALLENGE 
-# Using the .sort_values() method, can you find the degrees with the highest potential? Find the top 5 degrees with the highest values in the 90th percentile.  
-# Also, find the degrees with the greatest spread in salaries. Which majors have the largest difference between high and low earners after graduation. 
+# CODING CHALLENGE
+# Using the .sort_values() method, can you find the degrees with the highest potential? Find the top 5 degrees with the highest values in the 90th percentile.
+# Also, find the degrees with the greatest spread in salaries. Which majors have the largest difference between high and low earners after graduation.
 
 # SOLUTION
 # Majors with the Highest Potential
-    # highest_potential = clean_df.sort_values('Mid-Career 90th Percentile Salary', ascending=False)
-    # highest_potential[['Undergraduate Major', 'Mid-Career 90th Percentile Salary']].head()
+# highest_potential = clean_df.sort_values('Mid-Career 90th Percentile Salary', ascending=False)
+# highest_potential[['Undergraduate Major', 'Mid-Career 90th Percentile Salary']].head()
 # Majors with the Greatest Spread in Salaries
-    # highest_spread = clean_df.sort_values('Spread', ascending=False)
-    # highest_spread[['Undergraduate Major', 'Spread']].head()
+# highest_spread = clean_df.sort_values('Spread', ascending=False)
+# highest_spread[['Undergraduate Major', 'Spread']].head()
 
 
 # Grouping and Pivoting Data with Pandas (like MS Excel PIVOT!)
 # Use the .groupby() method
 
+# Learning Points & Summary
+# Use .head(), .tail(), .shape and .columns to explore your DataFrame
+# Look for NaN (not a number) values with .findna() and consider using .dropna() to clean up your DataFrame.
+# You can access entire columns of a DataFrame using the square bracket notation: df['column name'] or df[['column name 1', 'column name 2', 'column name 3']]
+# You can access individual cells in a DataFrame by chaining square brackets df['column name'][index] or using df['column name'].loc[index]
+# The largest and smallest values, as well as their positions, can be found with methods like .max(), .min(), .idxmax() and .idxmin()
+# You can sort the DataFrame with .sort_values() and add new columns with .insert()
+# To create an Excel Style Pivot Table by grouping entries that belong to a particular category use the .groupby() method
