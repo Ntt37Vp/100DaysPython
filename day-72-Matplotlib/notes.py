7cf796d9219d2c3486a8f5f830f3fe72f368b13e
Ntt37Vp <fuckspammers@protonmail.com>
2023-05-19 21:39:38 +0800

    day 72 matplotlib

---
+++ day-72-Matplotlib/notes.py
@@ -0,0 +1,8 @@
+
+# CHALLENGES
+# Read the .csv file and store it in a Pandas DataFrame called df. Have a look at the read_csv() documentation and try to provide these column names: ['DATE', 'TAG', 'POSTS']
+# Look at the first and last 5 rows of the DataFrame.
+# How many rows and how many columns does it have?
+# Count the number of entries in each column.
+
+# Solutions
