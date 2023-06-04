6bd1344725ad6c91d2433a11187f2f32ab86c574
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-04 12:53:31 +0800

    day 73 - sec 563

--- day-73/notes.py
+++ day-73/notes.py
@@ -9,3 +9,11 @@
     # colors_df['name'].nunique()
     # colors_df.groupby('is_trans').count()
     # colors_df.is_trans.value_counts()
+
+    # Challenge: Read the sets.csv data and take a look at the first and last couple of rows.
+        # sets_df = pd.read_csv('data/sets.csv')
+        # sets_df.head()
+        # sets_df.tail()
+
+    # Challenge: In which year were the first LEGO sets released and what were these sets called?
+        # sets_df.sort_values('year').head()
