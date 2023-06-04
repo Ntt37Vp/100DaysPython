5d3825196e1c7af28d0c5c5206b9532a1ae33717
Ntt37Vp <fuckspammers@protonmail.com>
2023-06-04 12:44:45 +0800

    day 73 minor edits

--- day-73/notes.py
+++ day-73/notes.py
@@ -2,4 +2,10 @@
 
 # Import
     # import pandas as pd
+
 # Data Exploration
+    # df = pd.read_csv("data/colors.csv")
+    # colors_df.head()
+    # colors_df['name'].nunique()
+    # colors_df.groupby('is_trans').count()
+    # colors_df.is_trans.value_counts()
